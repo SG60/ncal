5433199e085a50375e89298e4db40416c6580ec6
Sam Greening <samjg60@gmail.com>
2021-12-30 22:31:57 +0000

    Formatting with `black`
    formatting
    
    comment out second calendar

--- GCalToken.py
+++ GCalToken.py
@@ -3,15 +3,12 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 import pickle
 from datetime import datetime, timedelta, date
 
-#SET UP THE GOOGLE CALENDAR API INTERFACE
+# SET UP THE GOOGLE CALENDAR API INTERFACE
 
 
-scopes = ['https://www.googleapis.com/auth/calendar']
+scopes = ["https://www.googleapis.com/auth/calendar"]
 flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
 credentials = flow.run_console()
 pickle.dump(credentials, open("token.pkl", "wb"))
 credentials = pickle.load(open("token.pkl", "rb"))
 service = build("calendar", "v3", credentials=credentials)
-
-
-
