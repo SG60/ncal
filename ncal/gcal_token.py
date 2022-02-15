f55b0f6e88792f5555f7677b260932e658c93db5
Sam Greening <2552620+SG60@users.noreply.github.com>
2022-02-15 21:02:07 +0000

    test(config): add a couple of doctests
    docs(config): code formatting in docstring
    
    docs(config): proper docstring for env_var_names_dict
    
    style(config): flake8 noqa

--- ncal/gcal_token.py
+++ ncal/gcal_token.py
@@ -2,8 +2,6 @@ import pickle
 
 from google_auth_oauthlib.flow import InstalledAppFlow  # type: ignore
 
-# from googleapiclient.discovery import build  # type: ignore
-
 # SET UP THE GOOGLE CALENDAR API INTERFACE
 
 
@@ -12,5 +10,3 @@ def gcal_token(credentials_location, client_secret_file):
     flow = InstalledAppFlow.from_client_secrets_file(client_secret_file, scopes=scopes)
     credentials = flow.run_console()
     pickle.dump(credentials, open(credentials_location, "wb"))
-    # credentials = pickle.load(open("token.pkl", "rb"))
-    # service = build("calendar", "v3", credentials=credentials)
